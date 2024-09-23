58d7ddd0141d849cb301f8fc1df68f9ebf2580fe
caale18 <crodriguezd10@miumg.edu.gt>
2024-09-23 07:58:02 -0600

    conexion de todos los productos a la BD

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,4 @@
+const product = require('../models/product');
 const Product = require('../models/product')
 
 // Create new product => /app/v1/product/new
@@ -12,9 +13,34 @@ exports.newProduct = async (req, res, next) => {
 }
 
 
-exports.getProducts = (req, res, next) => {
+//Obtener todos los productos => /api/v1/products
+exports.getProducts = async (req, res, next) => {
+
+    const products = await Product.find();
+
+
     res.status(200).json({
         success: true,
-        message: 'This route will show all products in database'
+        count: products.length,
+        products
+    })
+}
+
+//Obtener detalles de un solo producto => /api/v1/product/:id
+
+exports.getSingleProduct = async (req, res, next) => {
+
+    const product = await Product.findById(req.params.id)
+
+    if(!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'Producto no encontrado'
+        })
+    }
+    
+    res.status(200).json({
+        success: true,
+        product
     })
 }
