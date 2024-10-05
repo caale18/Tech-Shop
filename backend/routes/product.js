6d0978e14dc8d0379714e5a6630853b3ebe5e757
caale18 <crodriguezd10@miumg.edu.gt>
2024-10-04 23:37:03 -0600

    Permiso en roles

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -10,13 +10,15 @@ const {
 
 } = require('../controllers/productController')
 
+const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
+
 router.route('/products').get(getProducts);
 router.route('/product/:id').get(getSingleProduct);
 
-router.route('/Admin/product/new').post(newProduct);
+router.route('/Admin/product/new').post(isAuthenticatedUser, authorizeRoles('admin'), newProduct);
 
 router.route('/admin/product/:id')
-    .put(updateProduct)
-    .delete(deleteProduct);
+    .put(isAuthenticatedUser, authorizeRoles('admin'), updateProduct)
+    .delete(isAuthenticatedUser, authorizeRoles('admin'), deleteProduct);
 
 module.exports = router;
